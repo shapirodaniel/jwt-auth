274c2caae36db47c83eccc4043b9e4cd7e689d16
shapirodaniel <shapirodanieladam@gmail.com>
2021-04-14 17:19:19 -0500

    bugfix: regenerate register mutation, was using login logic

--- web/src/generated/graphql.tsx
+++ web/src/generated/graphql.tsx
@@ -1,89 +1,81 @@
 import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-const defaultOptions =  {}
+export type Exact<T extends { [key: string]: unknown }> = {
+	[K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
+	{ [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
+	{ [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions = {};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
+	ID: string;
+	String: string;
+	Boolean: boolean;
+	Int: number;
+	Float: number;
 };
 
 export type LoginResponse = {
-  __typename?: 'LoginResponse';
-  accessToken: Scalars['String'];
+	__typename?: 'LoginResponse';
+	accessToken: Scalars['String'];
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
-  register: Scalars['Boolean'];
-  revokeRefreshTokensForUser: Scalars['Boolean'];
-  login: LoginResponse;
+	__typename?: 'Mutation';
+	register: Scalars['Boolean'];
+	revokeRefreshTokensForUser: Scalars['Boolean'];
+	login: LoginResponse;
 };
-
 
 export type MutationRegisterArgs = {
-  password: Scalars['String'];
-  email: Scalars['String'];
+	password: Scalars['String'];
+	email: Scalars['String'];
 };
-
 
 export type MutationRevokeRefreshTokensForUserArgs = {
-  userId: Scalars['Int'];
+	userId: Scalars['Int'];
 };
 
-
 export type MutationLoginArgs = {
-  password: Scalars['String'];
-  email: Scalars['String'];
+	password: Scalars['String'];
+	email: Scalars['String'];
 };
 
 export type Query = {
-  __typename?: 'Query';
-  hello: Scalars['String'];
-  bye: Scalars['String'];
-  users: Array<User>;
+	__typename?: 'Query';
+	hello: Scalars['String'];
+	bye: Scalars['String'];
+	users: Array<User>;
 };
 
 export type User = {
-  __typename?: 'User';
-  id: Scalars['Int'];
-  email: Scalars['String'];
+	__typename?: 'User';
+	id: Scalars['Int'];
+	email: Scalars['String'];
 };
 
-export type HelloQueryVariables = Exact<{ [key: string]: never; }>;
+export type HelloQueryVariables = Exact<{ [key: string]: never }>;
 
-
-export type HelloQuery = (
-  { __typename?: 'Query' }
-  & Pick<Query, 'hello'>
-);
+export type HelloQuery = { __typename?: 'Query' } & Pick<Query, 'hello'>;
 
 export type RegisterMutationVariables = Exact<{
-  email: Scalars['String'];
-  password: Scalars['String'];
+	email: Scalars['String'];
+	password: Scalars['String'];
 }>;
 
-
-export type RegisterMutation = (
-  { __typename?: 'Mutation' }
-  & { login: (
-    { __typename?: 'LoginResponse' }
-    & Pick<LoginResponse, 'accessToken'>
-  ) }
-);
-
+export type RegisterMutation = { __typename?: 'Mutation' } & Pick<
+	Mutation,
+	'register'
+>;
 
 export const HelloDocument = gql`
-    query Hello {
-  hello
-}
-    `;
+	query Hello {
+		hello
+	}
+`;
 
 /**
  * __useHelloQuery__
@@ -100,25 +92,39 @@ export const HelloDocument = gql`
  *   },
  * });
  */
-export function useHelloQuery(baseOptions?: Apollo.QueryHookOptions<HelloQuery, HelloQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<HelloQuery, HelloQueryVariables>(HelloDocument, options);
-      }
-export function useHelloLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<HelloQuery, HelloQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<HelloQuery, HelloQueryVariables>(HelloDocument, options);
-        }
+export function useHelloQuery(
+	baseOptions?: Apollo.QueryHookOptions<HelloQuery, HelloQueryVariables>
+) {
+	const options = { ...defaultOptions, ...baseOptions };
+	return Apollo.useQuery<HelloQuery, HelloQueryVariables>(
+		HelloDocument,
+		options
+	);
+}
+export function useHelloLazyQuery(
+	baseOptions?: Apollo.LazyQueryHookOptions<HelloQuery, HelloQueryVariables>
+) {
+	const options = { ...defaultOptions, ...baseOptions };
+	return Apollo.useLazyQuery<HelloQuery, HelloQueryVariables>(
+		HelloDocument,
+		options
+	);
+}
 export type HelloQueryHookResult = ReturnType<typeof useHelloQuery>;
 export type HelloLazyQueryHookResult = ReturnType<typeof useHelloLazyQuery>;
-export type HelloQueryResult = Apollo.QueryResult<HelloQuery, HelloQueryVariables>;
+export type HelloQueryResult = Apollo.QueryResult<
+	HelloQuery,
+	HelloQueryVariables
+>;
 export const RegisterDocument = gql`
-    mutation Register($email: String!, $password: String!) {
-  login(email: $email, password: $password) {
-    accessToken
-  }
-}
-    `;
-export type RegisterMutationFn = Apollo.MutationFunction<RegisterMutation, RegisterMutationVariables>;
+	mutation Register($email: String!, $password: String!) {
+		register(email: $email, password: $password)
+	}
+`;
+export type RegisterMutationFn = Apollo.MutationFunction<
+	RegisterMutation,
+	RegisterMutationVariables
+>;
 
 /**
  * __useRegisterMutation__
@@ -138,10 +144,21 @@ export type RegisterMutationFn = Apollo.MutationFunction<RegisterMutation, Regis
  *   },
  * });
  */
-export function useRegisterMutation(baseOptions?: Apollo.MutationHookOptions<RegisterMutation, RegisterMutationVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument, options);
-      }
+export function useRegisterMutation(
+	baseOptions?: Apollo.MutationHookOptions<
+		RegisterMutation,
+		RegisterMutationVariables
+	>
+) {
+	const options = { ...defaultOptions, ...baseOptions };
+	return Apollo.useMutation<RegisterMutation, RegisterMutationVariables>(
+		RegisterDocument,
+		options
+	);
+}
 export type RegisterMutationHookResult = ReturnType<typeof useRegisterMutation>;
 export type RegisterMutationResult = Apollo.MutationResult<RegisterMutation>;
-export type RegisterMutationOptions = Apollo.BaseMutationOptions<RegisterMutation, RegisterMutationVariables>;
+export type RegisterMutationOptions = Apollo.BaseMutationOptions<
+	RegisterMutation,
+	RegisterMutationVariables
+>;
