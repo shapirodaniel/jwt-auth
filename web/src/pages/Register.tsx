7f0b7d8ba11155961ab3b2bf206ab9f4b47f9454
shapirodaniel <shapirodanieladam@gmail.com>
2021-04-14 17:08:51 -0500

    add: user registration logic

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
+import { useRegisterMutation } from '../generated/graphql';
 
 interface Props {}
 
 export const Register: React.FC<Props> = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [register] = useRegisterMutation();
 
 	return (
 		<form
-			onSubmit={e => {
+			onSubmit={async e => {
 				e.preventDefault();
 
 				console.log('form submitted');
-				console.log('email, password: ', email, password);
+
+				const response = await register({
+					variables: {
+						email,
+						password,
+					},
+				});
+
+				console.log(response);
 			}}
 		>
 			<div>
